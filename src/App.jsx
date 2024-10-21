50179408df2725a45b37551bf40277381c0b9aaa
[raghu] <raghufks@gmail.com>
2024-10-21 12:23:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+import HeroSection from "./components/Hero";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
